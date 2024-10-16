85e51ae51a968be538e32dce72c8ad71cfae4546
BhargavA29 <bhargavayare@gmail.com>
2024-10-16 11:55:21 +0530

    Clerk Authentication Setup

--- app/(landingPage)/_components/Heading.tsx
+++ app/(landingPage)/_components/Heading.tsx
@@ -1,21 +1,46 @@
 "use client"
 
+import { Spinner } from "@/components/spinner";
 import { Button } from "@/components/ui/button";
+import { SignInButton } from "@clerk/nextjs";
+import { useConvexAuth } from "convex/react";
 import { ArrowRight } from "lucide-react";
+import Link from "next/link";
+
 
 export const Heading = () => {
+
+    const { isAuthenticated, isLoading } = useConvexAuth();
+
     return (
-        <div  className="max-w-3xl space-y-4">
+        <div className="max-w-3xl space-y-4">
             <h1 className="text-3xl sm:text-5xl sm:text-6xl font-bold">
                 Your Ideas, Documents & Plans. Unified. Welcome to <span className="underline">Notion</span>
             </h1>
             <h3 className="text-base sm:text-xl md:text-2xl font-medium">
                 Notion is the connected workspace where better, faster work happens.
             </h3>
-            <Button className="justify-between">
-                Enter Notion
-                <ArrowRight className="w-4 h-4 ml-2" />
-            </Button>
+            {isLoading && (
+                <div className="w-full flex items-center justify-center">
+                    <Spinner size="lg" />
+                </div>
+            )}
+            {isAuthenticated && !isLoading && (
+                <Button asChild>
+                    <Link href="/documents">
+                        Enter Notion
+                        <ArrowRight className="w-4 h-4 ml-2" />
+                    </Link>
+                </Button>
+            )}
+            {!isAuthenticated && !isLoading && (
+                <SignInButton mode="modal">
+                    <Button>
+                        Get Notion free
+                        <ArrowRight className="w-4 h-4 ml-2" />
+                    </Button>
+                </SignInButton>
+            )}
         </div>
     );
 }
