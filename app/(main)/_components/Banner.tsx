c7ec058c7f315b2d66befec8bccaf94787bb0811
BhargavA29 <bhargavayare@gmail.com>
2024-10-18 19:12:25 +0530

    Added Banner component for Archived Documents

---
+++ app/(main)/_components/Banner.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { api } from "@/convex/_generated/api";
+import { Id } from "@/convex/_generated/dataModel";
+import { useMutation } from "convex/react";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
+import { ConfirmModel } from "@/components/models/confirm-model";
+
+interface BannerProps {
+    documentId: Id<"documents">;
+}
+
+export const Banner = ({ documentId }: BannerProps) => {
+    const router = useRouter();
+    const remove = useMutation(api.documents.remove);
+    const restore = useMutation(api.documents.restore);
+
+    const onRemove = () => {
+        const promise = remove({ id: documentId });
+
+        toast.promise(promise, {
+            loading: "Moving to trash...",
+            success: "Note moved to trash!",
+            error: "Error moving to trash!",
+        });
+        router.push("/documents");
+    };
+
+    const onRestore = () => {
+        const promise = restore({ id: documentId });
+
+        toast.promise(promise, {
+            loading: "Restoring note...",
+            success: "Note restored!",
+            error: "Error restoring note!",
+        });
+    }
+
+    return (
+        <div className=" flex bg-rose-500 text-right text-sm p-2 text-white items-center gap-x-2 justify-center">
+            <p>
+                This page is in the Trash.
+            </p>
+            <Button
+                size="sm"
+                onClick={onRestore}
+                variant="outline"
+                className=" border-white bg-transparent hover:bg-primary/5 text-white hover:text-white p-1 px-2 h-auto font-normal"
+            >
+                Restore page
+            </Button>
+            <ConfirmModel
+                onConfirm={onRemove}
+            >
+                <Button
+                    size="sm"
+
+                    variant="outline"
+                    className=" border-white bg-transparent hover:bg-primary/5 text-white hover:text-white p-1 px-2 h-auto font-normal"
+                >
+                    Delete Forever
+                </Button>
+            </ConfirmModel>
+        </div>
+    );
+}
--- app/(main)/_components/Navbar.tsx
+++ app/(main)/_components/Navbar.tsx
@@ -2,10 +2,14 @@
 
 import { useQuery } from "convex/react";
 import { useParams } from "next/navigation";
-import { Id } from "@/convex/_generated/dataModel";
 import { MenuIcon } from "lucide-react";
+
+import { Id } from "@/convex/_generated/dataModel"
 import { api } from "@/convex/_generated/api";
+
 import { Title } from "./Title";
+import { Banner } from "./Banner";
+import { Menu } from "./Menu";
 
 interface NavbarProps {
     isCollapsed: boolean;
@@ -22,6 +26,9 @@ const Navbar = ({ isCollapsed, onResetWidth }: NavbarProps) => {
         return (
             <nav className="bg-background dark:bg-[#1F1F1F] px-3 py-1.5 w-full flex items-center justify-between">
                 <Title.Skeleton />
+                <div className="flex items-center gap-x-2">
+                    <Menu.Skeleton />
+                </div>
             </nav>
         );
     }
@@ -32,7 +39,7 @@ const Navbar = ({ isCollapsed, onResetWidth }: NavbarProps) => {
     }
     return (
         <>
-            <nav className="bg-background dark:bg-[#1F1F1F] px-3 py-2 w-full flex items-center gap-x-4">
+            <nav className="bg-background dark:bg-[#1F1F1F] px-3 py-2 w-full flex items-center justify-between gap-x-4">
                 {isCollapsed && (
                     <MenuIcon
                         onClick={onResetWidth}
@@ -42,10 +49,16 @@ const Navbar = ({ isCollapsed, onResetWidth }: NavbarProps) => {
                 )}
                 <div className="flex items-center justify-between w-full">
                     <Title initialData={document} />
+                    <div className="flex items-center gap-x-2">
+                        <Menu documentId={document._id} />
+                    </div>
 
 
                 </div>
             </nav>
+            {document.isArchived && (
+                <Banner documentId={document._id} />
+            )}
         </>
     );
 }
